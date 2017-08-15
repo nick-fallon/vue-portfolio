8c076fc1d83d2c4977cbb3ab541e2aa38d9f5dac
Nicholas Fallon <nickfallonwastaken@gmail.com>
2017-08-15 00:56:51 -0600

    Navbar

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template id="">
+  <div>
+    <h1>About Me</h1>
+    <ul class="nav nav-tabs">
+    <li role="presentation" class="active"><a href="#">Home</a></li>
+    <li role="presentation"><a href="#">Profile</a></li>
+    <li role="presentation"><a href="#">Messages</a></li>
+  </ul>
+</div>
+</template>
+
+<script type="text/javascript">
+
+</script>
+
+<style media="screen">
+
+</style>

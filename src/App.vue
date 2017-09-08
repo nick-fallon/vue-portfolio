c10b5d3660bc6d916e8aa3422940d79bfd5980e5
Nicholas Fallon <nickfallonwastaken@gmail.com>
2017-09-08 15:47:52 -0600

    drawer pops out

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,59 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <v-app>
+      <v-navigation-drawer absolute persistent light v-model="drawer" overflow>
+        <v-toolbar flat>
+          <v-list class="pa-0">
+            <v-list-tile avatar>
+              <v-list-tile-avatar>
+                <img src="https://randomuser.me/api/portraits/men/85.jpg" />
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>Nick Fallon</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-toolbar>
+        <v-list class="pt-0" dense>
+          <v-divider></v-divider>
+          <v-list-tile v-for="item in items" :key="item.title" @click="">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar fixed class="cyan darken-4" dark>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>Nick Fallon</v-toolbar-title>
+      </v-toolbar>
+      <main>
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
+      </main>
+    </v-app>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+      return {
+        drawer: true,
+        items: [
+          { title: 'Home', icon: 'fa-home' },
+          { title: 'About', icon: 'fa-question' },
+          { title: 'Projects', icon: 'fa-flask' },
+          { title: 'Contact Me', icon: 'fa-comment-o' }
+        ],
+        right: null
+      }
+    }
 }
 </script>
 
@@ -14,6 +61,9 @@ export default {
 body {
   margin: 0;
   font-family: 'Josefin Sans', Helvetica, sans-serif;
+}
+.container {
+  padding: 0;
 }
 
 </style>
